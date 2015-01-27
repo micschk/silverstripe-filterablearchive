c1fbecb2baf08d0dd7ff464c65af58f0e4cbf3c8
micschk <mvanschaik@gmail.com>
2015-01-27 21:59:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+Filterable archive - filter pages by date, tag or category
+==========================================================
+
+This module decorates pages with configurable fields to filter them by. Handy for newsitems & blogs, etc.
+(Largely adapted & decoupled from micmania1/silverstripe-blogger)
+
+*Filter items via dropdowns by date.*
+![](images/screenshots/todo.png)
+
+
+## Features
+
+* Apply decorators to filter pages by date (year, month/year, or day/month/year)
+
+
+## Todo
+
+* Filtering by Tags & Categories & corresponding extensions
+
+
+## Installation
+
+```
+composer require micschk/silverstripe-filterablearchive dev-master
+```
+
+Then add the decorators & criteria (todo) which you want to filter by to config.yml:
+
+```yaml
+---
+name: 'filterablearchiveextensions'
+---
+NewsHolder:
+  extensions:
+    - 'FilterableArchiveHolderExtension'
+  pagination_control_tab: "Root.Subpages" # name of tab for pagination settings, defaults to "Root.Main"
+  pagination_insert_before: "Subpages" # optional, set position of tab
+  managed_object_class: "NewsItem"
+  managed_object_date_field: "Date"
+NewsHolder_Controller:
+  extensions:
+    - 'FilterableArchiveHolder_ControllerExtension'
+NewsItem_Controller:
+  extensions:
+    - 'FilterableArchiveItemExtension'
+```
+
+
+## Recommended
+
+Newsgrid module (manage newsitems from a gridfield), applies this module
+* [silverstripe-newsgrid](https://github.com/micschk/silverstripe-newsgrid)
+
+
+## Requirements
+
+* SilverStripe 3.0 or newer
---
+++ code/FilterableArchiveHolder_ControllerExtension.php
@@ -0,0 +1,127 @@
+<?php
+
+class FilterableArchiveHolder_ControllerExtension extends Extension {
+
+	private static $allowed_actions = array(
+		'archive',
+	);
+
+	private static $url_handlers = array(
+		'archive/$Year!/$Month/$Day' => 'archive',
+	);
+	
+	/**
+	 * Renders an archive for a specificed date. This can be by year or year/month
+	 *
+	 * @return SS_HTTPResponse
+	**/
+	public function archive() {
+		$year = $this->owner->getArchiveYear();
+		$month = $this->owner->getArchiveMonth();
+		$day = $this->owner->getArchiveDay();
+
+		// If an invalid month has been passed, we can return a 404.
+		if($this->owner->request->param("Month") && !$month) {
+			return $this->owner->httpError(404, "Not Found");
+		}
+
+		// Check for valid day
+		if($month && $this->owner->request->param("Day") && !$day) {
+			return $this->owner->httpError(404, "Not Found");
+		}
+
+		if($year) {
+			$this->owner->Items = $this->owner->getFilteredArchiveItems($year, $month, $day);
+			return $this->owner->render();
+		} else {
+			return $this->owner->redirect($this->owner->AbsoluteLink(), 303); //301: movedperm, 302: movedtemp, 303: see other
+		}
+	}
+	
+	/**
+	 * Returns a list of paginated blog posts based on the blogPost dataList
+	 *
+	 * @return PaginatedList
+	**/
+	public function PaginatedItems() {
+		
+		$items = new PaginatedList($this->owner->Items, $this->owner->request);
+		// If pagination is set to '0' then no pagination will be shown.
+		if($this->owner->ItemsPerPage > 0) $items->setPageLength($this->owner->ItemsPerPage);
+		else $items->setPageLength($this->owner->getItems()->count());
+		return $items;
+
+	}
+	
+	/**
+	 * Fetches the archive year from the url
+	 *
+	 * @return int|null
+	**/
+	public function getArchiveYear() {
+		$year = $this->owner->request->param("Year");
+		if(preg_match("/^[0-9]{4}$/", $year)) {
+			return (int) $year;
+		}
+		return null;
+	}
+
+	/**
+	 * Fetches the archive money from the url.
+	 *
+	 * @return int|null
+	**/
+	public function getArchiveMonth() {
+		$month = $this->owner->request->param("Month");
+		if(preg_match("/^[0-9]{1,2}$/", $month)) {
+			if($month > 0 && $month < 13) {
+				// Check that we have a valid date.
+				if(checkdate($month, 01, $this->owner->getArchiveYear())) {
+					return (int) $month;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Fetches the archive day from the url
+	 *
+	 * @return int|null
+	**/
+	public function getArchiveDay() {
+		$day = $this->owner->request->param("Day");
+		if(preg_match("/^[0-9]{1,2}$/", $day)) {
+
+			// Check that we have a valid date
+			if(checkdate($this->owner->getArchiveMonth(), $day, $this->owner->getArchiveYear())) {
+				return (int) $day;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the current archive date.
+	 *
+	 * @return Date
+	**/
+	public function getArchiveDate() {
+		$year = $this->owner->getArchiveYear();
+		$month = $this->owner->getArchiveMonth();
+		$day = $this->owner->getArchiveDay();
+
+		if($year) {
+			if($month) {
+				$date = $year . '-' . $month . '-01';
+				if($day) {
+					$date = $year . '-' . $month . '-' . $day;
+				}
+			} else {
+				$date = $year . '-01-01';
+			}
+			return DBField::create_field("Date", $date);
+		}
+	}
+	
+}
